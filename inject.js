b210e3a22e76c29041bb3aa377c98a405f8701e8
Jonathan Muth <contact@jonathanmuth.com>
2015-05-23 21:32:06 +0200

    Add Download Buttons and remove faulty JS

--- inject.js
+++ inject.js
@@ -1,7 +1,13 @@
 $(document).ready(function() {
   $('div').safeReplace('commits', 'revisions');
-});
-
-$(document).bind("DOMSubtreeModified", function() {
-  $('div').safeReplace('commits', 'revisions');
+  $(".only-with-full-nav").prepend ( '                \
+  \
+  <div id="gmSomeID" style="width:100%"><h3 style="margin-bottom: 1em;"><span class="text-emphasized">Download</span> options</h3><a href="http://www.onlineagenten.de/" class="btn btn-sm sidebar-button" aria-label="Download the contents of stefanibus/stefanibus.github.io as a zip file" title="Download the contents of stefanibus/stefanibus.github.io as a zip file" rel="nofollow"><span class="octicon octicon-cloud-download"></span> Download PDF</a><a href="http://www.onlineagenten.de/" class="btn btn-sm sidebar-button" aria-label="Download the contents of stefanibus/stefanibus.github.io as a zip file" title="Download the contents of stefanibus/stefanibus.github.io as a WORD file" rel="nofollow"><span class="octicon octicon-cloud-download"></span> Download WORD</a><a href="http://www.onlineagenten.de/" class="btn btn-sm sidebar-button" aria-label="Download the contents of stefanibus/stefanibus.github.io as a PDF file" title="Download the contents of stefanibus/stefanibus.github.io as a PDF file" rel="nofollow"><span class="octicon octicon-cloud-download"></span> Download TXT</a></p>       \
+  \
+      </div>                          \
+  ' );
+  $(".commit-title").append ( '                \
+  <a style="float:right; height:27px; top:-5px; right: -5px;" href="http://www.onlineagenten.de/" class="btn btn-sm sidebar-button" aria-label="Download the contents of stefanibus/stefanibus.github.io as a zip file" title="Download the contents of stefanibus/stefanibus.github.io as a zip file" rel="nofollow"><span class="octicon octicon-cloud-download"></span> Download PDF</a> \
+                                     \
+  ' );
 });
